17941f4a75d629ccc00b0aba27fa717916718aaf
DVZ <polo1215@bk.ru>
2022-08-07 20:14:09 +0300

    add Example 005 If Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "dmitry")
+{
+    Console.WriteLine("Hi!!! My Friend Dmitry");
+}
+else
+{
+    Console.Write("Привет! ");
+    Console.WriteLine(username );
+}
