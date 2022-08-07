1faaf7f010f9fbc7c985773c6c27ed738771f621
DVZ <polo1215@bk.ru>
2022-08-07 19:57:59 +0300

    add Example 3.1 Random

---
+++ Example003.1_Sum_Random/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10); // Случайный целые числа от 1,2,3....до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int sum = numberA + numberB;
+Console.WriteLine(sum);
