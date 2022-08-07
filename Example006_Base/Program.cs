3ae606a6124451333c064b2568ad5b96254ff681
DVZ <polo1215@bk.ru>
2022-08-07 20:25:43 +0300

    add Example 006 Maximum Finding

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 2;
+int b = 5;
+int c = 3;
+int d = 10;
+int e = 12;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("Максимум равен: ");
+Console.WriteLine(max);
