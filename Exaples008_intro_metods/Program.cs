316c01fc6079f0c932bdbe41d30c5d449efd626f
DVZ <polo1215@bk.ru>
2022-08-08 18:09:34 +0300

    Lesson-2 Add funnction

--- Exaples008_intro_metods/Program.cs
+++ Exaples008_intro_metods/Program.cs
@@ -1,4 +1,12 @@
-﻿int a1 = 2;
+﻿int Max(int arg1, int arg2, int arg3)
+{
+   int res = arg1;
+   if (arg2 > res)  res = arg2;
+   if (arg3 > res)  res = arg3;
+   return res;
+}
+
+int a1 = 2;
 int b1 = 3;
 int c1 = 4;
 int a2 = 44;
@@ -8,15 +16,9 @@ int a3 = 44;
 int b3 = 44;
 int c3 = 777;
 
-int max = a1;
-if (b1 > max) max = b1;
-if (c1 > max) max = c1;
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
 
-if (a2 > max) max = a2;
-if (b2 > max) max = b2;
-if (c3 > max) max = c3;
-
-if (a3 > max) max = a3;
-if (b3 > max) max = b3;
-if (c3 > max) max = c3;
 Console.WriteLine(max);
