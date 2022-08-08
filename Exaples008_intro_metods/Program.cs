88bb47bed656c8b996e11ab50c38df16a1cd48d3
DVZ <polo1215@bk.ru>
2022-08-08 18:19:22 +0300

    Lesson-2 оттимизировали код

--- Exaples008_intro_metods/Program.cs
+++ Exaples008_intro_metods/Program.cs
@@ -10,15 +10,19 @@ int a1 = 2;
 int b1 = 3;
 int c1 = 4;
 int a2 = 44;
-int b2 = 34;
+int b2 = 3455;
 int c2 = 66;
 int a3 = 44;
 int b3 = 44;
 int c3 = 777;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
 
+int max = Max(
+            Max(a1, b1, c1), 
+            Max(a2, b2, c2), 
+            Max(a3, b3, c3));
 Console.WriteLine(max);
