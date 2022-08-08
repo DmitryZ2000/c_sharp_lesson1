f6fd8c254157930b3f4ab97c30ebd8c343bf2cf0
DVZ <polo1215@bk.ru>
2022-08-08 18:55:12 +0300

    Lesson-2 Finding element of array

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 31, 23, 32, 34, 56, 73, 83, 93, 03, 32 };
+
+int n = array.Length; // возвращает длину массива
+int find = 32;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; // index = index +1;
+}
