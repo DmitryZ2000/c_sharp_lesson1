50f0a2bc926c39ca7d539cc9e2aca86b00496364
DVZ <orionvega5@gmail.com>
2022-08-15 19:17:50 +0300

    Add new Method Lesson3

---
+++ Example_lesson3/Program.cs
@@ -0,0 +1,24 @@
+﻿void Method1()
+{
+    Console.WriteLine("Я автор....");
+}
+// Method1(); //Не забываем писать скобки после вызова метода.
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Мой текст");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg: "My Text", count: 4);
+Console.WriteLine();
+Method21(count: 4, msg: "Новое сообщение"); //Использование именнованных аргументов, позволяет передавать значения не попорядку
