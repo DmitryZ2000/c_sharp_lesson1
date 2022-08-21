3c02cd455fc68c40b1438fee6d8f308850989c6b
Dmitry <orionvega5@gmail.com>
2022-08-22 00:23:40 +0300

    Add Lesson4 Fibonacci

---
+++ Example013Lesson4Fibonacci/Program.cs
@@ -0,0 +1,10 @@
+﻿double Fibonacci(int n)
+{
+    if (n == 0 || n == 1) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n - 2);
+}
+
+for (int i = 0; i < 50; i++)
+{
+    Console.WriteLine($"{i} = {Fibonacci(i)}");
+}
