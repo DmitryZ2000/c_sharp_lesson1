41eb3dd874a905841346952519d9679fdcf4da40
Dmitry <orionvega5@gmail.com>
2022-08-28 16:12:22 +0300

    Получили массив данных

--- Example014Lesson6/Program.cs
+++ Example014Lesson6/Program.cs
@@ -6,14 +6,11 @@ string text = "(1,2) (2,3) (4,5) (6,7)"
 Console.WriteLine(text);
 var data = text.Split(" ")
                 .Select(item => item.Split(','))
+                .Select(e=> (int.Parse(e[0]),int.Parse(e[1])))
                 .ToArray();
 
 for (int i = 0; i < data.Length; i++)
 {
-    // Console.WriteLine(data[i]);
-    for (int k = 0; k < data[i].Length; k++)
-    {
-        Console.WriteLine(data[i][k]);
-    }
+    Console.WriteLine(data[i]);
     Console.WriteLine();
 }
